703bd2ff9270465a97bb81379e9de2052e0d4425
Andrew Rodland <andrew@cleverdomain.org>
2019-03-06 21:33:39 -0500

    Add a DifferenceModel

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,4 +2,4 @@ from .gp3d import GP3DModel
 from .sph import SPHModel
 from .irispline import IRISplineModel
 
-from .combinators import HybridModel, LogSpaceModel, ProductModel, LinearModel
+from .combinators import HybridModel, LogSpaceModel, ProductModel, LinearModel, DifferenceModel
--- app/models/combinators.py
+++ app/models/combinators.py
@@ -50,3 +50,13 @@ class ProductModel:
         model1_pred = self.model1.predict(longitude, latitude)
         model2_pred = self.model2.predict(longitude, latitude)
         return model1_pred * model2_pred
+
+class DifferenceModel:
+    def __init__(self, model1, model2):
+        self.model1 = model1
+        self.model2 = model2
+
+    def predict(self, longitude, latitude):
+        model1_pred = self.model1.predict(longitude, latitude)
+        model2_pred = self.model2.predict(longitude, latitude)
+        return model1_pred - model2_pred
