a624c2d946e41b5ece7f134591bde867a9aa01d2
Andrew Rodland <andrewr@vimeo.com>
2019-06-26 13:25:38 -0400

    Switch the basis function to Matern (nu=3/2, length scale=1)
    This is motivated by Guinness and Fuentes 2016
    (doi:10.1016/j.jmva.2015.08.018) which shows that using the "chordal
    distance" on a sphere embedded in 3D space is well-founded (yay!) but
    that covariance functions other than Matern fall over sometimes. mat32
    gives a pretty good result, which appears to fit the data points better
    *and* be globally smoother, so let's go with it.

--- app/models/gp3d.py
+++ app/models/gp3d.py
@@ -18,7 +18,7 @@ class GP3DModel:
         x, y, z = sph_to_xyz(df['station.longitude'].values, df['station.latitude'].values)
         stdev = 0.7 - 0.5 * df.cs
 
-        gp = rbf.gauss.gpiso(rbf.basis.se, (0.0, 1.0, 1.2))
+        gp = rbf.gauss.gpiso(rbf.basis.mat32, (0.0, 1.0, 1.0))
         self.gp = gp_cond = gp.condition(np.vstack((x,y,z)).T, t, sigma=stdev)
 
     def predict(self, longitude, latitude):
