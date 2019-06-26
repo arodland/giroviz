eebd0738bb01db752bd49e7ee4d3cdb4a686be7f
Andrew Rodland <andrew@cleverdomain.org>
2019-06-25 23:23:30 -0400

    Get rid of that high-frequency component in the GP model
    It gives better localized fitting to the stations, but it just makes
    everything less stable overall.

--- app/models/gp3d.py
+++ app/models/gp3d.py
@@ -18,7 +18,7 @@ class GP3DModel:
         x, y, z = sph_to_xyz(df['station.longitude'].values, df['station.latitude'].values)
         stdev = 0.7 - 0.5 * df.cs
 
-        gp = 0.33 * rbf.gauss.gpiso(rbf.basis.se, (0.0, 1.0, 0.3)) + 0.67 * rbf.gauss.gpiso(rbf.basis.se, (0.0, 1.0, 1.2))
+        gp = rbf.gauss.gpiso(rbf.basis.se, (0.0, 1.0, 1.2))
         self.gp = gp_cond = gp.condition(np.vstack((x,y,z)).T, t, sigma=stdev)
 
     def predict(self, longitude, latitude):
